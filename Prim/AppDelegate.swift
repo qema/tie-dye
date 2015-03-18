06ee40b1283d00e783baad56779898efbf9af0c2
Andrew Wang <andrew@wanganzhou.com>
2015-03-18 14:28:34 -0700

    Initial Commit

---
+++ Prim/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Prim
+//
+//  Created by Andrew Wang on 3/18/15.
+//  Copyright (c) 2015 Andrew Wang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Prim/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Prim
+//
+//  Created by Andrew Wang on 3/18/15.
+//  Copyright (c) 2015 Andrew Wang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
